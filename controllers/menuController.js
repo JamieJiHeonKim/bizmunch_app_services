8b1427004287f3ee3cfebe9721a555d7c4bc5e11
JamieJiHeonKim <jamiejiheonkim@gmail.com>
2024-09-21 16:06:58 -0400

    now accepts multiple images for restaurant and menu

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -3,17 +3,77 @@ const Menu = require('../models/menuModel');
 
 const getMenu = async (req, res) => {
     try {
-        const restaurantId = req.params.id;
-
-        const menu = await Menu.findOne({ restaurantId: new mongoose.Types.ObjectId(restaurantId) });
-
-        if (!menu) {
-            return res.status(404).json({ message: 'There is no menu available for this restaurant.' });
+        const gfs = req.app.locals.gfs;
+        if (!gfs) {
+            return res.status(500).json({ message: 'GridFS is not initialized' });
         }
 
-        res.json(menu);
+        const menus = await Menu.find({ restaurantId: req.params.id });
+
+        const menusWithFiles = await Promise.all(menus.map(async (menu) => {
+            for (let category in menu.menu) {
+                const items = Object.entries(menu.menu[category]);
+
+                for (const [key, item] of items) {
+                    if (item.barcode) {
+                        const barcodeFile = await gfs.find({ _id: new mongoose.Types.ObjectId(item.barcode) }).toArray();
+                        if (!barcodeFile || barcodeFile.length === 0) {
+                            throw new Error(`No file found for barcode ID: ${item.barcode}`);
+                        }
+
+                        const barcodeStream = gfs.openDownloadStream(barcodeFile[0]._id);
+                        const barcodeChunks = [];
+
+                        barcodeStream.on('data', (chunk) => {
+                            barcodeChunks.push(chunk);
+                        });
+
+                        const barcodeBuffer = await new Promise((resolve, reject) => {
+                            barcodeStream.on('end', () => {
+                                resolve(Buffer.concat(barcodeChunks));
+                            });
+
+                            barcodeStream.on('error', (err) => {
+                                reject(err);
+                            });
+                        });
+
+                        item.barcode = `data:image/png;base64,${barcodeBuffer.toString('base64')}`;
+                    }
+
+                    if (item.image) {
+                        const imageFile = await gfs.find({ _id: new mongoose.Types.ObjectId(item.image) }).toArray();
+                        if (!imageFile || imageFile.length === 0) {
+                            throw new Error(`No file found for image ID: ${item.image}`);
+                        }
+
+                        const imageStream = gfs.openDownloadStream(imageFile[0]._id);
+                        const imageChunks = [];
+
+                        imageStream.on('data', (chunk) => {
+                            imageChunks.push(chunk);
+                        });
+
+                        const imageBuffer = await new Promise((resolve, reject) => {
+                            imageStream.on('end', () => {
+                                resolve(Buffer.concat(imageChunks));
+                            });
+
+                            imageStream.on('error', (err) => {
+                                reject(err);
+                            });
+                        });
+
+                        item.image = `data:image/png;base64,${imageBuffer.toString('base64')}`;
+                    }
+                }
+            }
+            return menu;
+        }));
+
+        res.json(menusWithFiles[0]);
     } catch (error) {
-        // console.error('Error fetching menu:', error);
+        console.error('Error fetching menu:', error);
         res.status(500).json({ message: 'Failed to fetch menu' });
     }
 };
--- models/menuModel.js
+++ models/menuModel.js
@@ -5,22 +5,20 @@ const menuItemSchema = new Schema({
     name: { type: String, required: true },
     price: { type: String, required: true },
     calories: { type: String },
-    ingredients: { type: [String] },
-    image: { type: String }
-});
+    description: { type: String },
+    barcode: { type: String, required: false },
+    discount: { type: Boolean, default: false, required: true },
+    image: { type: String, required: false }
+  });
 
-const menuSchema = new Schema({
+  const menuSchema = new Schema({
     restaurantId: { type: Schema.Types.ObjectId, ref: 'Restaurant', required: true },
-    menu: { 
-        type: Map, 
-        of: new Schema({
-            price: { type: String, required: true },
-            calories: { type: String },
-            ingredients: { type: [String] }, 
-        }), 
-        default: {} 
+    restaurantName: { type: String, required: true },
+    menu: {
+      type: Object,
+      default: {},
     },
-}, { timestamps: true });
+  }, { timestamps: true });
 
 const Menu = mongoose.model('Menu', menuSchema);
 module.exports = Menu;
